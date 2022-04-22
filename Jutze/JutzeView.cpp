0f9f96e057afdc0358c52049235a032e0dc82b55
Yuan_Qin <yuan.qin@jutze.com.cn>
2022-04-22 17:37:40 +0800

    New Test

---
+++ Jutze/JutzeView.cpp
@@ -0,0 +1,105 @@
+﻿
+// JutzeView.cpp: CJutzeView 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "Jutze.h"
+#endif
+
+#include "JutzeDoc.h"
+#include "JutzeView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CJutzeView
+
+IMPLEMENT_DYNCREATE(CJutzeView, CView)
+
+BEGIN_MESSAGE_MAP(CJutzeView, CView)
+	// 标准打印命令
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CView::OnFilePrintPreview)
+END_MESSAGE_MAP()
+
+// CJutzeView 构造/析构
+
+CJutzeView::CJutzeView() noexcept
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CJutzeView::~CJutzeView()
+{
+}
+
+BOOL CJutzeView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CJutzeView 绘图
+
+void CJutzeView::OnDraw(CDC* /*pDC*/)
+{
+	CJutzeDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// CJutzeView 打印
+
+BOOL CJutzeView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 默认准备
+	return DoPreparePrinting(pInfo);
+}
+
+void CJutzeView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加额外的打印前进行的初始化过程
+}
+
+void CJutzeView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加打印后进行的清理过程
+}
+
+
+// CJutzeView 诊断
+
+#ifdef _DEBUG
+void CJutzeView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CJutzeView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CJutzeDoc* CJutzeView::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CJutzeDoc)));
+	return (CJutzeDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CJutzeView 消息处理程序
